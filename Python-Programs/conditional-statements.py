8b79654be17eadd0135a78178a78b6f0a2d19429
Lokesh-Reddy-Kambham <kambhamlokeshreddy@gmail.com>
2025-09-11 00:12:57 +0530

    Add conditional-statements.py: if-elif-else examples

---
+++ Python-Programs/conditional-statements.py
@@ -0,0 +1,22 @@
+shashank = int(input("Enter Number:"))
+rahul = int(input("Enter Number:"))
+if shashank > rahul:
+    print("Shashank Won!")
+elif rahul > shashank:
+    print("Rahul is Cheating!")
+else:
+    print("you given Tough competition rahul")
+
+
+a = int(input("Enter Number:"))
+b = int(input("Enter Number:"))
+if a > b:
+    print("a is Greater")
+elif b > a:
+    print("b is Greater")
+else :
+    print("both are equal")
+
+
+
+
