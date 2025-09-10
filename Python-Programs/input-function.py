88c38e21ec848e3086aefd373edc1eca4875658c
Lokesh-Reddy-Kambham <kambhamlokeshreddy@gmail.com>
2025-09-11 00:11:41 +0530

    Add input-function.py: example of user input handling

---
+++ Python-Programs/input-function.py
@@ -0,0 +1,12 @@
+#input()
+a = input("Enter Num:")
+b = input("Enter Num:")
+res = a + b
+print(res)
+
+# input() with int(): to convert input into int
+a = int(input("Enter Num:"))
+b = int(input("Enter Num:"))
+result = a + b
+print(result)
+
