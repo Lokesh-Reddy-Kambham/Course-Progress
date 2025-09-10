8c60ae90f2eb4dfe8beb569da9e966924f9da5de
Lokesh-Reddy-Kambham <kambhamlokeshreddy@gmail.com>
2025-09-11 00:10:07 +0530

    Add variables-datatypes.py: variables and data types in Python

---
+++ Python-Programs/variables-datatypes.py
@@ -0,0 +1,17 @@
+a:int = 10
+b:int = 10
+c:int = 10
+d:int = 20
+print(id(a))
+print(id(b))
+print(id(c))
+print(id(d))
+
+# b=20
+# print(id(b))
+l1 = [1,2,3,4]
+l2 = [1,2,3,4]
+print(l1 is l2)
+print(id(l1))
+print(id(l2))
+
