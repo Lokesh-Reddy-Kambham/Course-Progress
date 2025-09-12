7167ec7db50b04f5c9e56a3500495c15133c9189
Lokesh-Reddy-Kambham <kambhamlokeshreddy@gmail.com>
2025-09-12 22:47:12 +0530

    add for-loop.py:for loop program

---
+++ Python-Programs/for-loop.py
@@ -0,0 +1,30 @@
+for num in 1,2,3,4,5,6,7,8,99,100,57:
+    print(num)
+
+for i in 1,4,9,16,36,64,"Eighty One":
+    print(i)
+
+for j in 1,2,3,4,5,6,7,7,8,"Vasco De Gama".split():
+    print(j)
+
+for x in range(17):
+    print(x)
+
+for y in range(1,17):
+    print(y)
+
+for i in "Rama","Sita":
+    print(i)
+for i in "Rama":
+    print(i,end="") #End parameter without anything
+for i in range(10):
+    print(i,end=" ")
+print()
+for i in range(1,10):
+    print(i,end=" ")
+print()
+for i in range(1,50,2):
+    print(i,end=" ")
+print()
+for i in range(2,50,2):
+    print(i,end=" ")
