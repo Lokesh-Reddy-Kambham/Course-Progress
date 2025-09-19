7ce5d1e2d89f81bd0ac49b1d4cd5acb0ff71b8df
Lokesh-Reddy-Kambham <kambhamlokeshreddy@gmail.com>
2025-09-19 15:43:49 +0530

    add Static-and-Class-method.py:Example of @staticmethod and @classmethod

---
+++ Python-Programs/Static-and-Class-method.py
@@ -0,0 +1,18 @@
+class Mobile:
+    def __init__(self):
+        self.brand="Redmi"
+    def call(self):
+        print("Incoming call from Lover!!!")
+    @staticmethod
+    def game():
+        print("Uppi is Gamer")
+    @classmethod
+    def sleep(cls):
+        print("Uppi is sleeping")
+
+m1= Mobile()
+m1.call()
+# m1.game()      """Not a standard way and not Recommended"""
+# m1.sleep()
+Mobile.game()
+Mobile.sleep()
