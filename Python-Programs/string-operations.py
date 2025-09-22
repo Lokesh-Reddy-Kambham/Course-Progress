f3c2dcbfd37694c54667402fc031b36515d67e34
Lokesh-Reddy-Kambham <kambhamlokeshreddy@gmail.com>
2025-09-23 00:07:00 +0530

    added string-operations.py : Examples of String operations

---
+++ Python-Programs/string-operations.py
@@ -0,0 +1,17 @@
+str1 = "Uppi "
+str2 = "Khatyu"
+
+str3 = str1 + str2
+print(str3)
+
+# str4 = str1 - str2
+# print(str4)
+
+str5 = str1 * 2
+print(str5)
+
+# str6 = str1 / str2
+# print(str6)
+
+# str7 = str1 * str2
+# print(str7)
