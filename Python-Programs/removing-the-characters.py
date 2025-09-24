36ce5fb91900ca55d1251bf8f04084a9eaaebeb6
Lokesh-Reddy-Kambham <kambhamlokeshreddy@gmail.com>
2025-09-25 00:52:43 +0530

    Removing the characters from string

---
+++ Python-Programs/removing-the-characters.py
@@ -0,0 +1,15 @@
+str1 = "   Nagu is dancing   "
+print(str1)
+
+str2 = str1.lstrip("-")
+print(str2)
+str3 = str1.rstrip("-")
+print(str3)
+
+str4 = str1.strip("-")
+print(str4)
+print("------------")
+st5 = str1.replace(" ","")
+print(st5)
+print("---------------")
+
