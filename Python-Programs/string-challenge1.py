50a7de014f120dacee5406e90abcd06a4fecf90f
Lokesh-Reddy-Kambham <kambhamlokeshreddy@gmail.com>
2025-09-25 01:01:21 +0530

    Challenge in String

---
+++ Python-Programs/string-challenge1.py
@@ -0,0 +1,13 @@
+string = " U p p e ndr a"
+print(string)
+str1 = ""
+for i in string:
+    if i==" ":
+        pass
+    else :
+        str1= str1+i
+print(str1)
+
+
+str2 = string.replace(" ","")
+print(str2)
