ab87b3a6fefac6f4d492fcf67fcf731259dd340b
Lokesh-Reddy-Kambham <kambhamlokeshreddy@gmail.com>
2025-09-28 22:26:28 +0530

    Example of types of functions

---
+++ Python-Programs/types-of-functions.py
@@ -0,0 +1,23 @@
+def add():
+    a = 10
+    b = 20
+    c = a+b
+    print(c)
+add()
+def subtract():
+    a = 30
+    b = 20
+    c = a-b
+    return c
+print(subtract())
+def multiply(a,b):
+    c = a*b
+    print(c)
+multiply(5,7)
+
+def division(a,b):
+    c = a/b
+    return c
+print(division(45,9))
+
+
