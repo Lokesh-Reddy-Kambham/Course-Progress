2148e5369ee7bb3bba7706b4de19fb6d4638e599
Lokesh-Reddy-Kambham <kambhamlokeshreddy@gmail.com>
2025-09-28 21:42:52 +0530

    Example of string Case Conversion into Upper ,lower

---
+++ Python-Programs/string-cases.py
@@ -0,0 +1,11 @@
+string = input("Enter a String:")
+print(string)
+
+upper_case = string.upper()
+print(upper_case)
+
+lower_case = string.lower()
+print(lower_case)
+
+swap_case = string.swapcase()
+print(swap_case)
