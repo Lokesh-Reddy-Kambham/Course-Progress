5b6194309365286c91dc998eba4c6918a5c95262
Lokesh-Reddy-Kambham <kambhamlokeshreddy@gmail.com>
2025-10-06 16:50:17 +0530

    talking list of numbers using while loop

---
+++ Python-Programs/list-of-numbers.py
@@ -0,0 +1,18 @@
+l = []
+i = 0
+while i<=4:
+    print("Enter a num:")
+    num = int(input())
+    l.insert(0,num)
+    i = i+1
+    print(i)
+print(l)
+
+
+i = 0
+while i<=4:
+    print(l[i])
+    i = i + 1
+
+
+
