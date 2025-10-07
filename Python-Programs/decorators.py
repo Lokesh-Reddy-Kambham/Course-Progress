7c3574ad58779db40c4f1e354c5d54acc685f4db
Lokesh-Reddy-Kambham <kambhamlokeshreddy@gmail.com>
2025-10-08 00:06:38 +0530

    Decorators Example

---
+++ Python-Programs/decorators.py
@@ -0,0 +1,31 @@
+def main():
+    print("Inside main")
+def outer(ptr):
+    print("Inside outer")
+    def inner():
+        print("Inside inner")
+        ptr1 = ptr
+        ptr1()
+        print("Leaving Inner")
+    return inner()
+outer(main)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
