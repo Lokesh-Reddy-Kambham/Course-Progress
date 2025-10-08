3bf89a711c1d9994d5e460a41bf8789877d47da1
Lokesh-Reddy-Kambham <kambhamlokeshreddy@gmail.com>
2025-10-08 16:13:42 +0530

    added another Example of Decorators

--- Python-Programs/decorators.py
+++ Python-Programs/decorators.py
@@ -10,7 +10,18 @@ def outer(ptr):
     return inner()
 outer(main)
 
-
+def main():
+    print("Inside main")
+def outer(ptr):
+    print("Inside outer")
+    def inner():
+        print("Inside inner")
+        ptr1 = ptr
+        ptr1()
+        print("Leaving Inner")
+    return inner
+ref = outer(main)
+ref()
 
 
 
