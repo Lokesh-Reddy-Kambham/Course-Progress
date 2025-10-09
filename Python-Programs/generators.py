c61476ed388e68da40bfb4b1ce74a1fbc997f3a4
Lokesh-Reddy-Kambham <kambhamlokeshreddy@gmail.com>
2025-10-09 22:36:48 +0530

    Generators

---
+++ Python-Programs/generators.py
@@ -0,0 +1,13 @@
+# def gr():
+#     yield 1
+#     yield 2
+#     yield 3
+# res = gr()
+# # print(list(res))
+# # print(next(res))
+# # print(next(res))
+# # print(next(res))
+# print(iter(res))
+
+help("class")
+
