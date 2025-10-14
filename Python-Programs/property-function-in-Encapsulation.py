b3f4c0b4f320dabddeb0fb2ebc1abef378074791
Lokesh-Reddy-Kambham <kambhamlokeshreddy@gmail.com>
2025-10-14 11:11:31 +0530

    using property function accessed and modified encapsulated variables

--- Python-Programs/property-function-in-Encapsulation.py
+++ Python-Programs/property-function-in-Encapsulation.py
@@ -5,7 +5,7 @@ class Person:
         return self.__name
     def setter(self,val):
         self.__name = val
-    get_set = property(getter,setter)
+    get_set = property(getter,setter)#property method used in encapsulation
 p1 = Person()
 p1.get_set = "lokesh"
 res = p1.getter()
