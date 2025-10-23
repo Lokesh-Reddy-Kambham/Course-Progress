a38516bcd9899c6dd7b1d24065f52d1112ea3172
Lokesh-Reddy-Kambham <kambhamlokeshreddy@gmail.com>
2025-10-23 13:14:19 +0530

    introduction to dictionary

---
+++ Python-Programs/dictionary.py
@@ -0,0 +1,13 @@
+emp_info = {"id":45,"name":"sundri","age":16}
+print(emp_info)
+print(type(emp_info))
+print(len(emp_info))
+
+emp_info["addr"]= "Bangalore"
+print(emp_info)
+
+emp_info["name"]="sundra"
+print(emp_info)
+
+del emp_info["age"]
+print(emp_info)
