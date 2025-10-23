59e901c3b2fde81bb848d0b135efff1aeaac5bb9
Lokesh-Reddy-Kambham <kambhamlokeshreddy@gmail.com>
2025-10-23 14:10:06 +0530

    iterating dictionary and some methods

---
+++ Python-Programs/iterating-dictionary.py
@@ -0,0 +1,13 @@
+student = {"name":"Dhanush","gender":"male","addr":"Russia"}
+print(student["addr"])
+for i in student:
+    print(i)
+for i in student:
+    print(student[i])
+for i in student.keys():
+    print(i)
+for i in student.values():
+    print(i)
+for i,j in student.items():
+    print(i,":",j)
+
