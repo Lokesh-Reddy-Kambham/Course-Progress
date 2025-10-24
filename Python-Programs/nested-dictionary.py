e89bf4eccebcd15d36c681e905f753289bef8398
Lokesh-Reddy-Kambham <kambhamlokeshreddy@gmail.com>
2025-10-24 13:21:02 +0530

    nested dictionary

---
+++ Python-Programs/nested-dictionary.py
@@ -0,0 +1,6 @@
+customer = {"name":"lokesh","age":23,"phone":{"jio":143,"airtel":5711},"address":{"perm":"tirupati","res":"BTM"}}
+print(customer)
+print(customer["age"])
+print(customer["phone"]["airtel"])
+print(customer["address"]["perm"])
+
