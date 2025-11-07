f277c40b7546ce46fdf09f94075521410719cd10
Lokesh-Reddy-Kambham <kambhamlokeshreddy@gmail.com>
2025-11-07 23:49:23 +0530

    Else Block in Error Handling

---
+++ Core-Python/Exception-Handling/error-handling-5.py
@@ -0,0 +1,10 @@
+a = int(input("Enter a Num:"))
+b = int(input("Enter a Num:"))
+try:
+    res = a//b
+    print(res)
+except Exception as e :
+    print("Error Occured")
+else :
+    print("Program runs SuccessFully!")
+
