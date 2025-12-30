5848438deaeb2650683b9a09a19396274490b007
Lokesh-Reddy-Kambham <kambhamlokeshreddy@gmail.com>
2025-12-30 11:25:44 +0530

    Arithmetic operators

---
+++ WebTech/Javascript/arithmetic-operations.js
@@ -0,0 +1,25 @@
+function add(){
+    num1 = Number(document.getElementById("num1").value)
+    num2 = Number(document.getElementById("num2").value)
+    alert(num1+num2);   
+    console.log(num1+num2);
+    
+}
+function subtract(){
+    num1 = Number(document.getElementById("num1").value)
+    num2 = Number(document.getElementById("num2").value)
+    alert(num1-num2);
+    console.log(num1-num2);
+}
+function multiply(){
+    num1 = Number(document.getElementById("num1").value)
+    num2 = Number(document.getElementById("num2").value)
+    alert(num1*num2);
+    console.log(num1*num2);
+}
+function division(){
+    num1 = Number(document.getElementById("num1").value)
+    num2 = Number(document.getElementById("num2").value)
+    alert(num1/num2);
+    console.log(num1/num2);
+}
