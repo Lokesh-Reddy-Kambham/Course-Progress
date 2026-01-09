dcf44a913ca31fbb906f17a5423f03c82ef923fd
Lokesh-Reddy-Kambham <kambhamlokeshreddy@gmail.com>
2026-01-09 10:15:19 +0530

    looping with arrays

---
+++ WebTech/Javascript/looping-with-array.js
@@ -0,0 +1,9 @@
+let cast = ["Dustin","Lucas","Will","Mike"]
+//for of loop
+for(let i of cast){
+    console.log(i);
+} 
+//for Each loop
+cast.forEach((value,index)=>{
+    console.log(`${index} : ${value}`);
+});
